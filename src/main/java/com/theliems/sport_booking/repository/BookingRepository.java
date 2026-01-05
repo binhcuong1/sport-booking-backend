4127b05668aec5c2484644b1519cf24a4ffe574c
vupham1211 <phamlevu1211@gmail.com>
2026-01-05 20:52:27 +0700

    API

--- src/main/java/com/theliems/sport_booking/repository/BookingRepository.java
+++ src/main/java/com/theliems/sport_booking/repository/BookingRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
 
     @Query("""
         SELECT (COUNT(b) > 0)
@@ -20,6 +23,75 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
             @Param("clubId") Integer clubId,
             @Param("status") BookingStatus status
     );
-    java.util.List<Booking> findByProfileIdOrderByCreatedAtDesc(Integer profileId);
-}
 
+
+    @Query("""
+        SELECT
+            b.bookingId,
+            c.clubName,
+            b.totalTime,
+            b.totalPrice,
+            b.paymentMethod,
+            b.note,
+            b.bookingStatus,
+            b.createdAt
+        FROM Booking b
+        JOIN Club c ON b.clubId = c.clubId
+        WHERE b.profileId = :profileId
+        ORDER BY b.createdAt DESC
+    """)
+    List<Object[]> findUserBookingsWithClubName(
+            @Param("profileId") Integer profileId
+    );
+
+
+    @Query("""
+        SELECT
+            b.bookingId,
+            c.clubName,
+            b.totalTime,
+            b.totalPrice,
+            b.paymentMethod,
+            b.note,
+            b.bookingStatus,
+            b.createdAt
+        FROM Booking b
+        JOIN Club c ON b.clubId = c.clubId
+        WHERE b.bookingId = :bookingId
+    """)
+    Object[] findBookingDetailWithClubName(
+            @Param("bookingId") Integer bookingId
+    );
+
+
+    @Query("""
+        SELECT
+            b.bookingId,
+            c.clubName,
+            b.profileId,
+            b.totalTime,
+            b.bookingStatus,
+            b.createdAt
+        FROM Booking b
+        JOIN Club c ON b.clubId = c.clubId
+        WHERE b.clubId = :clubId
+        ORDER BY b.createdAt DESC
+    """)
+    List<Object[]> findBookingsWithClubName(
+            @Param("clubId") Integer clubId
+    );
+
+    @Query("""
+        SELECT
+            b.bookingId,
+            c.clubName,
+            b.profileId,
+            b.totalTime,
+            b.bookingStatus,
+            b.createdAt
+        FROM Booking b
+        JOIN Club c ON b.clubId = c.clubId
+        ORDER BY b.createdAt DESC
+    """)
+    List<Object[]> findAllBookingsWithClubName();
+}
